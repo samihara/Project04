a61503450b4165be867c2fbe023a24aef75febcb
Samiha Rahman <samiha.ra08@gmail.com>
2024-12-13 21:38:09 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,22 +4,16 @@ const path = require('path');
 const apiRoutes = require('./routes/api');
 const uiRoutes = require('./routes/ui');
 
-
 const app = express();
 const PORT = 3000;
 
-
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
-
-// Routes
 app.use('/api', apiRoutes);
 app.use('/ui', uiRoutes);
 
-
-// Start server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
 
